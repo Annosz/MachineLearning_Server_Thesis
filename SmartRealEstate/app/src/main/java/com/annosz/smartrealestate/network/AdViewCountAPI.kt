db57b052532a45e201d3673a624f8e7b71f1d295
Annosz <tadamleanyfalu@gmail.com>
2019-02-12 21:16:42 +0100

    Connect to Heroku

--- SmartRealEstate/app/src/main/java/com/annosz/smartrealestate/network/AdViewCountAPI.kt
+++ SmartRealEstate/app/src/main/java/com/annosz/smartrealestate/network/AdViewCountAPI.kt
@@ -4,16 +4,13 @@ import android.util.JsonReader
 import com.annosz.smartrealestate.model.AdvertisementData
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.*
 import java.util.concurrent.TimeUnit
 
 class AdViewCountAPI {
 
     companion object {
-        private const val BASE_URL = "https://demo8952134.mockable.io/AdViewCount"
+        private const val BASE_URL = "http://adviewcount.herokuapp.com/"
         private const val UTF_8 = "UTF-8"
         private const val TAG = "Network"
         private const val RESPONSE_ERROR = -1
@@ -34,11 +31,12 @@ class AdViewCountAPI {
         return response.body()?.string() ?: "EMPTY"
     }
 
-    private fun httpPost(url: String, body: RequestBody): String {
+    private fun httpPost(url: String, json: String): String {
+        val body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json)
+
         val request = Request.Builder()
                 .url(url)
                 .post(body)
-                .addHeader("Content-Type","application/json")
                 .build()
 
         //The execute call blocks the thread
@@ -48,29 +46,29 @@ class AdViewCountAPI {
 
     fun getView(ad: AdvertisementData): Int {
         return try {
-            var body = FormBody.Builder()
-                    .add("county", ad.county)
-                    .add("city", ad.city)
-                    .add("postcode", ad.postcode.toString())
-                    .add("property_type", ad.propertyType)
-                    .add("property_subtype", ad.propertySubtype)
-                    .add("property_condition_type", ad.propertyConditionType)
-                    .add("property_floor", ad.propertyFloor.toString())
-                    .add("building_floor_count", ad.buildingFloorCount.toString())
-                    .add("view_type", ad.viewType)
-                    .add("orientation", ad.orientation)
-                    .add("garden_access", ad.gardenAccess.toString())
-                    .add("heating_type", ad.heatingType)
-                    .add("elevator_type", ad.elevatorType.toString())
-                    .add("room_count", ad.roomCount.toString())
-                    .add("small_room_count", ad.smallRoomCount.toString())
-                    .add("property_area", ad.propertyArea.toString())
-                    .add("balcony_area", ad.balconyArea.toString())
-                    .add("price_created_at", ad.priceCreatedAt.toString())
-                    .add("active_days", "30")
-                    .build()
+            val json = """{
+                "county":"${ad.county}",
+                "city":"${ad.city}",
+                "postcode":"${ad.postcode.toString()}",
+                "property_type":"${ad.propertyType}",
+                "property_subtype":"${ad.propertySubtype}",
+                "property_condition_type":"${ad.propertyConditionType}",
+                "property_floor":"${ad.propertyFloor.toString()}",
+                "building_floor_count":"${ad.buildingFloorCount.toString()}",
+                "view_type":"${ad.viewType}",
+                "orientation":"${ad.orientation}",
+                "garden_access":"${ad.gardenAccess.toString()}",
+                "heating_type":"${ad.heatingType}",
+                "elevator_type":"${ad.elevatorType.toString()}",
+                "room_count":"${ad.roomCount.toString()}",
+                "small_room_count":"${ad.smallRoomCount.toString()}",
+                "property_area":"${ad.propertyArea.toString()}",
+                "balcony_area":"${ad.balconyArea.toString()}",
+                "price_created_at":"${ad.priceCreatedAt.toString()}",
+                "active_days":"30"
+                }""".trimIndent()
 
-            var response = httpPost(BASE_URL,body)
+            var response = httpPost(BASE_URL + "predict",json)
             if (response == "EMPTY")
             {
                 -1
